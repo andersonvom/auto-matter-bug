521f56ef75f528087f8b3e6c42e3f9be83cd8a44
Anderson Mesquita <andersonm@spotify.com>
2019-06-20 13:36:28 -0400

    Add error test case

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,15 +1,37 @@
 package com.mycompany.app;
 
-import org.junit.Assert;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.norberg.automatter.jackson.AutoMatterModule;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AppTest {
+  private ObjectMapper objectMapper;
+
+  @Before
+  public void setUp() {
+    objectMapper = new ObjectMapper().registerModule(new AutoMatterModule());
+  }
 
   @Test
-  public void testApp() {
-    Assert.assertTrue(true);
+  public void testApp() throws Exception {
+    Foobar foobar = new FoobarBuilder()
+        .foo("foo")
+        .bar("bar")
+        .build();
+
+    String json = objectMapper.writeValueAsString(foobar);
+    System.out.println("serialized: " + json);
+
+    // throws com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException:
+    //     Unrecognized field "empty" (class java.util.Optional),
+    //     not marked as ignorable (0 known properties: ])
+    Foobar parsed = objectMapper.readValue(json, Foobar.class);
+
+    System.out.println("foobar.foo: " + parsed.foo());
+    System.out.println("foobar.bar: " + parsed.bar().get());
   }
 }
